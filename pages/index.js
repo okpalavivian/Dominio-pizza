2deee8c7f60666f8376d0b1ea48e8486fa64a9a1
okpalavivian <109100020+okpalavivian@users.noreply.github.com>
2023-03-30 21:14:00 +0100

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Hero from '../components/Hero';
 import Layout from '../components/Layout';
 
 export default function Home() {
@@ -12,7 +13,7 @@ export default function Home() {
         </Head>
         {/* body */}
         <main>
-          <h1>Subscribe Zainkeepscode</h1>
+          <Hero />
         </main>
       </div>
     </Layout>
