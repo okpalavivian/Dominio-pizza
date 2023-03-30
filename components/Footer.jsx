4d66e6a8bebb7252d36ff6d596ff2d6ce59ea33c
okpalavivian <109100020+okpalavivian@users.noreply.github.com>
2023-03-30 09:33:52 +0100

    1 file modified

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,16 @@
 import css from '../styles/Footer.module.css';
+import { UilFacebook } from '@iconscout/react-unicons';
+import { UilGithub } from '@iconscout/react-unicons';
+import { UilInstagram } from '@iconscout/react-unicons';
 export default function Footer() {
   return (
     <div className={css.contianer}>
-      <span></span>
+      <span>ALL RIGHT RESERVED</span>
+      <div className={css.social}>
+        <UilFacebook />
+        <UilGithub />
+        <UilInstagram />
+      </div>
     </div>
   );
 }
