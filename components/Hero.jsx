1f1e81f81170e2670ce379253f001444a31f8744
okpalavivian <109100020+okpalavivian@users.noreply.github.com>
2023-03-30 22:20:18 +0100

    1 file modified, 1 added, 2 deleted

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,20 @@
 import css from '../styles/Hero.module.css';
+import Image from 'next/image';
+import Cherry from '../assets/Cherry.png';
 
-export default (params) => {
-  return(
-    <div>Hero</div>
-  )
+export default function Hero() {
+  return (
+    <div className={css.container}>
+      {/* left side*/}
+      <div className={css.cherry}>
+        <span>More than Faster</span>
+        <Image src={Cherry} alt="" width={40} height={25} />
+      </div>
+      {/*right side}
+
+
+       {/* right side*/}
+      <div className={css.rightSide}>1</div>
+    </div>
+  );
 }
