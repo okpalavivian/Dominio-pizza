50d7bd6abc4cadc0299d45d1c18ff01fe6b1503c
okpalavivian <109100020+okpalavivian@users.noreply.github.com>
2023-03-30 09:06:28 +0100

    3 files modified, 1 added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,8 @@
+import css from '../styles/Footer.module.css';
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={css.contianer}>
+      <span></span>
+    </div>
+  );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,29 @@
 import css from '../styles/Header.module.css';
 import Image from 'next/image';
 import Logo from '../assets/logo.png';
+import { UilShoppingBag } from '@iconscout/react-unicons';
 export default function Header() {
   return (
     <div className={css.header}>
       {/*logo side */}
       <div className={css.logo}>
-        <Image src={Logo} alt="" />
+        <Image src={Logo} alt="" width={50} height={50} />
+        <span>DOMINIO PIZZA</span>
+      </div>
+      {/*menu side */}
+
+      <ul className={css.menu}>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+
+      {/*right side*/}
+      <div className={css.rightSide}>
+        <div className={css.cart}>
+          <UilShoppingBag size={35} colour="#2e2e2e" />
+          <div className={css.badge}>1</div>
+        </div>
       </div>
     </div>
   );
